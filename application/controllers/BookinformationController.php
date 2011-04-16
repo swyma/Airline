07b879444acde6abf58dc47232437fee2dd4425e
Administrator <administrator@ubuntu.(none)>
2011-04-16 11:39:09 +0800

    modify

--- application/controllers/BookinformationController.php
+++ application/controllers/BookinformationController.php
@@ -1,18 +1,24 @@
 <?php
-
 class BookinformationController extends Zend_Controller_Action
 {
-
-    public function init()
+    public function init ()
     {
         /* Initialize action controller here */
     }
-
-    public function indexAction()
+    public function indexAction ()
     {
         // action body
     }
-	
-
+    public function showAction ()
+    {
+        // action body
+        //显示航班信息
+        $db = new Application_Model_DbTable_Flightinformation();
+        $flightinfo = "SELECT com_code,air_code,fli_everyday,fli_no,fli_discount,fli_baddress,fli_aaddress,SUBSTRING(fli_atime,12,5) as fli_atime, SUBSTRING(fli_btime,12,5) as fli_btime,fli_Ffare,fli_Yfare,fli_Cfare,fli_refundtime,fli_refund FROM flightinformation where flag=1";
+        $flightinformation = $db->getAllInfo($flightinfo);
+        $this->view->flightinformation = $flightinformation;
+    }
 }
+
+
 
--- application/models/DbTable/Flightinformation.php
+++ application/models/DbTable/Flightinformation.php
@@ -4,7 +4,12 @@ class Application_Model_DbTable_Flightinformation extends Zend_Db_Table_Abstract
 {
 
     protected $_name = 'flightinformation';
-
+	
+    public function getAllInfo($sql){
+    	$adapter = Zend_Registry::get('db');
+      	$flightinformation =  $adapter->query($sql);
+        return $flightinformation;
+    }
 
 }
 
